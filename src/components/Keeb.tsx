686b7870a83a37827fbe0805551b7bb3937ac072
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-11 14:05:03 -0700

    Add non letter keys

--- src/components/Keeb.tsx
+++ src/components/Keeb.tsx
@@ -2,6 +2,39 @@ export default function Keeb() {
     return (
         <div>
             <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">Esc</kbd>
+                <kbd className="kbd kbd-lg">F1</kbd>
+                <kbd className="kbd kbd-lg">F2</kbd>
+                <kbd className="kbd kbd-lg">F3</kbd>
+                <kbd className="kbd kbd-lg">F4</kbd>
+                <kbd className="kbd kbd-lg">F5</kbd>
+                <kbd className="kbd kbd-lg">F6</kbd>
+                <kbd className="kbd kbd-lg">F7</kbd>
+                <kbd className="kbd kbd-lg">F8</kbd>
+                <kbd className="kbd kbd-lg">F9</kbd>
+                <kbd className="kbd kbd-lg">F10</kbd>
+                <kbd className="kbd kbd-lg">F11</kbd>
+                <kbd className="kbd kbd-lg">F12</kbd>
+                <kbd className="kbd kbd-lg">PS</kbd>
+            </div>
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">`</kbd>
+                <kbd className="kbd kbd-lg">1</kbd>
+                <kbd className="kbd kbd-lg">2</kbd>
+                <kbd className="kbd kbd-lg">3</kbd>
+                <kbd className="kbd kbd-lg">4</kbd>
+                <kbd className="kbd kbd-lg">5</kbd>
+                <kbd className="kbd kbd-lg">6</kbd>
+                <kbd className="kbd kbd-lg">7</kbd>
+                <kbd className="kbd kbd-lg">8</kbd>
+                <kbd className="kbd kbd-lg">9</kbd>
+                <kbd className="kbd kbd-lg">0</kbd>
+                <kbd className="kbd kbd-lg">-</kbd>
+                <kbd className="kbd kbd-lg">+</kbd>
+                <kbd className="kbd kbd-lg">Backspace</kbd>
+            </div> 
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">Tab</kbd>
                 <kbd className="kbd kbd-lg">q</kbd>
                 <kbd className="kbd kbd-lg">w</kbd>
                 <kbd className="kbd kbd-lg">e</kbd>
@@ -12,8 +45,12 @@ export default function Keeb() {
                 <kbd className="kbd kbd-lg">i</kbd>
                 <kbd className="kbd kbd-lg">o</kbd>
                 <kbd className="kbd kbd-lg">p</kbd>
-                </div> 
-                <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">-</kbd>
+                <kbd className="kbd kbd-lg">-</kbd>
+                <kbd className="kbd kbd-lg">\</kbd>
+            </div> 
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">Caps</kbd>
                 <kbd className="kbd kbd-lg">a</kbd>
                 <kbd className="kbd kbd-lg">s</kbd>
                 <kbd className="kbd kbd-lg">d</kbd>
@@ -23,8 +60,12 @@ export default function Keeb() {
                 <kbd className="kbd kbd-lg">j</kbd>
                 <kbd className="kbd kbd-lg">k</kbd>
                 <kbd className="kbd kbd-lg">l</kbd>
-                </div> 
-                <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">;</kbd>
+                <kbd className="kbd kbd-lg">'</kbd>
+                <kbd className="kbd kbd-lg">Enter</kbd>
+            </div> 
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">Shift</kbd>
                 <kbd className="kbd kbd-lg">z</kbd>
                 <kbd className="kbd kbd-lg">x</kbd>
                 <kbd className="kbd kbd-lg">c</kbd>
@@ -32,7 +73,23 @@ export default function Keeb() {
                 <kbd className="kbd kbd-lg">b</kbd>
                 <kbd className="kbd kbd-lg">n</kbd>
                 <kbd className="kbd kbd-lg">m</kbd>
+                <kbd className="kbd kbd-lg">,</kbd>
+                <kbd className="kbd kbd-lg">.</kbd>
                 <kbd className="kbd kbd-lg">/</kbd>
+                <kbd className="kbd kbd-lg">Shift</kbd>
+                <kbd className="kbd kbd-lg">UP</kbd>
+            </div>
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">Ctrl</kbd>
+                <kbd className="kbd kbd-lg">Win</kbd>
+                <kbd className="kbd kbd-lg">Alt</kbd>
+                <kbd className="kbd kbd-lg">Space</kbd>
+                <kbd className="kbd kbd-lg">Alt</kbd>
+                <kbd className="kbd kbd-lg">Fn</kbd>
+                <kbd className="kbd kbd-lg">Ctrl</kbd>
+                <kbd className="kbd kbd-lg">LEFT</kbd>
+                <kbd className="kbd kbd-lg">DOWN</kbd>
+                <kbd className="kbd kbd-lg">RIGHT</kbd>
             </div>
         </div>
     )
