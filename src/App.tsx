0232aac926e96c737ccaee92a987c16409bff116
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-11 13:53:25 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Keeb from "./components/Keeb"
+
+function App() {
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <Keeb />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Keeb.tsx
@@ -0,0 +1,39 @@
+export default function Keeb() {
+    return (
+        <div>
+            <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">q</kbd>
+                <kbd className="kbd kbd-lg">w</kbd>
+                <kbd className="kbd kbd-lg">e</kbd>
+                <kbd className="kbd kbd-lg">r</kbd>
+                <kbd className="kbd kbd-lg">t</kbd>
+                <kbd className="kbd kbd-lg">y</kbd>
+                <kbd className="kbd kbd-lg">u</kbd>
+                <kbd className="kbd kbd-lg">i</kbd>
+                <kbd className="kbd kbd-lg">o</kbd>
+                <kbd className="kbd kbd-lg">p</kbd>
+                </div> 
+                <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">a</kbd>
+                <kbd className="kbd kbd-lg">s</kbd>
+                <kbd className="kbd kbd-lg">d</kbd>
+                <kbd className="kbd kbd-lg">f</kbd>
+                <kbd className="kbd kbd-lg">g</kbd>
+                <kbd className="kbd kbd-lg">h</kbd>
+                <kbd className="kbd kbd-lg">j</kbd>
+                <kbd className="kbd kbd-lg">k</kbd>
+                <kbd className="kbd kbd-lg">l</kbd>
+                </div> 
+                <div className="flex justify-center gap-1 my-1 w-full">
+                <kbd className="kbd kbd-lg">z</kbd>
+                <kbd className="kbd kbd-lg">x</kbd>
+                <kbd className="kbd kbd-lg">c</kbd>
+                <kbd className="kbd kbd-lg">v</kbd>
+                <kbd className="kbd kbd-lg">b</kbd>
+                <kbd className="kbd kbd-lg">n</kbd>
+                <kbd className="kbd kbd-lg">m</kbd>
+                <kbd className="kbd kbd-lg">/</kbd>
+            </div>
+        </div>
+    )
+}
